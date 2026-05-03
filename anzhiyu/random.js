72479c2e8bfdbbf370efc7895e00405e1cd18d7c
“lee945” <2717597548@qq.com>
2026-05-03 20:56:50 +0800

    Site updated: 2026-05-03 20:55:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/08/css-study/","2024/04/05/buu_misc1/","2024/04/09/buu_misc2/","2025/04/08/html-study/","2025/06/08/基于Windows控制台的入侵检测系统配置/","2025/04/10/js/","2024/11/20/buu_misc3/","2025/04/07/staller主题美化/","2025/04/07/test/","2025/04/07/xyctf_wp/","2025/11/05/next主题美化/"];function toRandomPost(){
+var posts=["2024/04/09/buu_misc2/","2025/04/08/css-study/","2024/04/05/buu_misc1/","2025/04/08/html-study/","2024/11/20/buu_misc3/","2025/04/07/staller主题美化/","2025/04/10/js/","2025/11/05/next主题美化/","2025/04/07/test/","2025/06/08/基于Windows控制台的入侵检测系统配置/","2025/04/07/xyctf_wp/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
